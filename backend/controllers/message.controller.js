7b90692e7c0bbd06fa0b173a6f922f7f4380acce
xbbbbbbbbbbbb <297906031@qq.com>
2025-05-02 16:35:54 +0200

    fourth part completed - socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
     try {
@@ -25,15 +26,19 @@ export const sendMessage = async (req, res) => {
 
         if(newMessage) {
             conversation.messages.push(newMessage._id);
-        }
-
-        // SOCKET IO FUNCTIONALITY
-        
+        }       
 
         // await conversation.save();
         // await newMessage.save();
         // this will run in parallel
         await Promise.all([conversation.save(), newMessage.save()]);
+
+        // SOCKET IO FUNCTIONALITY
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            // io.to().emit() is used to send events to specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage);
         
